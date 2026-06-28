fb7f9f91c24bf5d2258b91b15dda64f6189d0692
Dasun169 <dasunnavindu2001@gmail.com>
2026-06-28 15:10:13 +0530

    Add seperate timeouts here

---
+++ src/main/java/org/example/utils/Utils.java
@@ -0,0 +1,8 @@
+package org.example.utils;
+
+public class Utils {
+    private static final int DEFAULT_TIMEOUT = 10;
+    private static final int SHORT_TIMEOUT = 5;
+    private static final int LONG_TIMEOUT = 20;
+    private static final int MODERATE_TIMEOUT = 15;
+}
